c32f29fb36ef15ddc715d1a1f9bbd972145fd98d
Yan <me@octonezd.me>
2026-07-18 23:53:21 +0300

    bump version and fix alt store manifest generation

--- copy_xcodeproj.sh
+++ copy_xcodeproj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf xcode
 mkdir -p xcode
-cp -rv ~/love/platform/xcode/Images.xcassets xcode
-cp -rv ~/love/platform/xcode/ios/love-ios.plist xcode
-cp -rv ~/love/platform/xcode/vnds.xcodeproj xcode
+cp -rv ./love/platform/xcode/Images.xcassets xcode
+cp -rv ./love/platform/xcode/ios/love-ios.plist xcode
+cp -rv ./love/platform/xcode/vnds.xcodeproj xcode
